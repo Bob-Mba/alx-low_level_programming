0f2ea487bc742c4a2df5fec0cecb5882a84ac452
“Bob-Mba” <chiderabobmba@gmail.com>
2022-09-09 03:57:12 -0500

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
-	/* main- Entry point */
+	/* main - Entry point */
 	/*Return: always 0 (successful) */
 {
 	puts("\"Programming is like building a multilingual puzzle");
