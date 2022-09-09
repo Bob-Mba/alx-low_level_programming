59bc201f26fc6edb5345fa035dc7b98143dd2eeb
“Bob-Mba” <chiderabobmba@gmail.com>
2022-09-09 14:52:33 -0500

    tasl 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n>0)
 	{
-	    printf("is positive\n",n);
+	    printf("is positive\n");
 	}
 	    else if (n<0)
         {
-        printf("is negative\n",n);
+        printf("is negative\n");
         }
         else if (n==0)
-            printf("is zero\n",n);
+            printf("is zero\n");
 	return (0);
 }
